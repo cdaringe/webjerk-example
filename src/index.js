0ffc07f31020949160fdbdfdda12b39adb7f48ce
cdaringe <christopher.dieringer@gmail.com>
2017-03-29 14:39:35 -0700

    :game_die:

---
+++ src/index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var wj = require('webjerk')
+
+wj.run({
+  plugins: [
+    {
+      name: 'webjerk-snaps',
+      config: {
+        desiredCapabilities: [
+          { browserName: 'chrome' },
+          { browserName: 'firefox' }
+        ],
+        url: 'http://localhost:3333',
+        testName: 'bananas',
+        snapDefinitionsFromWindow: function snapDefinitionsFromWindow (divs, message) {
+          // @NOTE this JS is run in the _browser_ context
+          // webdriverio JS serialziation requires semis. :/
+          var divs = document.getElementsByTagName('div');
+          var map = [];
+          var i = 0;
+          var tDiv;
+          while (divs[i]) {
+            tDiv = divs[i];
+            if (!tDiv.id) tDiv.id = '__dummy_div_' + i;
+            map.push({ elem: '#' + tDiv.id, name: tDiv.id });
+            ++i;
+          }
+          return map;
+        }
+      }
+    }
+  ]
+})
